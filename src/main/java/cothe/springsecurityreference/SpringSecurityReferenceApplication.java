b1a2f50527a93b3134d3e31d044fef379368c1a0
Jeongjin Kim <thefuturecreator@gmail.com>
2018-05-15 18:20:36 +0900

    초기화 기본 웹 보안 설정

---
+++ src/main/java/cothe/springsecurityreference/SpringSecurityReferenceApplication.java
@@ -0,0 +1,12 @@
+package cothe.springsecurityreference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityReferenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityReferenceApplication.class, args);
+    }
+}
